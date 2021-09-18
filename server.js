24da682f85a22285970566c53c0dc74a9f517ca0
dkhang <khangb1906687@student.ctu.edu.vn>
2021-09-18 08:59:26 +0700

    Cai dat xu ly loi

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const config = require("./app/config");
+const { BadRequestError } = require("./app/helpers/errors");
 
 const app = express();
 
@@ -22,6 +23,19 @@ app.get("/",(req, res) => {
 });
 
 setupContactRoutes(app);
+
+//handle 404 response
+app.use((req, res, next) => {
+    next(new BadRequestError(404, "Resource not found"));
+});
+
+//define error-handing middleware last, atfer other app.use() and routes calls
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Sever Error",
+    });
+});
 
 //set port, listen for requests
 const PORT = config.app.port;
